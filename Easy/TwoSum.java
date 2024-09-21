fdc59c8f7a377f2550ab603e50cd1fca4b9e0150
LovisottoSantiago <santiagolovisotto@gmail.com>
2024-09-21 18:28:13 -0300

    Repository creation

---
+++ Easy/TwoSum.java
@@ -0,0 +1,24 @@
+
+
+public class TwoSum {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+
+    // Given an array of integers numbers and an integer target, return indices of the two numbers such that they add up to target.
+    // You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+    //Example
+    //Input: numbers = [2,7,11,15], target = 9
+    //Output: [0,1]
+    //Explanation: Because numbers[0] + numbers[1] == 9, we return [0, 1].
+
+    public int[] TwoSum(int[] numbers, int target){
+        int[] list = new int[] {2};
+        
+
+
+        return list;
+    }
+
+}
