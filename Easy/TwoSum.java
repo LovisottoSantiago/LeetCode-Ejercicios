2c7b8e73a163c9bd92b7dfbbf6c371d103de0d7d
LovisottoSantiago <santiagolovisotto@gmail.com>
2024-09-21 19:20:02 -0300

    TwoSum, nested loops.

--- Easy/TwoSum.java
+++ Easy/TwoSum.java
@@ -1,8 +1,18 @@
 
 
+
+
 public class TwoSum {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println("Two Sum");
+        /*int[] numbers = {2, 7, 11, 15};
+        int target = 9;*/
+
+        int[] numbers = {3, 3, 6};
+        int target = 9;
+
+        int[] newList = TwoSum(numbers, target);
+        System.out.println("[" + newList[0] + ", " + newList[1] + "].");
     }
 
     // Given an array of integers numbers and an integer target, return indices of the two numbers such that they add up to target.
@@ -13,12 +23,23 @@ public class TwoSum {
     //Output: [0,1]
     //Explanation: Because numbers[0] + numbers[1] == 9, we return [0, 1].
 
-    public int[] TwoSum(int[] numbers, int target){
-        int[] list = new int[] {2};
+    public static int[] TwoSum(int[] numbers, int target){
+        int[] list = new int[2];
         
-
-
+        for (int i=0; i < numbers.length -1; i++) {
+            for (int j=i + 1; j < numbers.length; j++) {
+                if (numbers[i] + numbers[j] == target) {
+                    list[0] = i;
+                    list[1] = j;
+                    return list;
+                }
+            }
+        }
         return list;
     }
+    // Due to these are nested loops, the time complexity is O(n x n) = O(n^2)
+    // Solution, use HashMap.
+
+
 
 }
