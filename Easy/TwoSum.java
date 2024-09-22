f6feacce291d6fa7740aee61d77330db334cdf0c
LovisottoSantiago <santiagolovisotto@gmail.com>
2024-09-22 14:49:42 -0300

    TwoSum with HashMap

--- Easy/TwoSum.java
+++ Easy/TwoSum.java
@@ -1,5 +1,6 @@
 
-
+import java.util.HashMap;
+import java.util.Map;
 
 
 public class TwoSum {
@@ -13,6 +14,8 @@ public class TwoSum {
 
         int[] newList = TwoSum(numbers, target);
         System.out.println("[" + newList[0] + ", " + newList[1] + "].");
+        int[] anotherList = TwoSumHashMap(numbers, target);
+        System.err.println("[" + anotherList[0] + ", " + anotherList[1] + "].");
     }
 
     // Given an array of integers numbers and an integer target, return indices of the two numbers such that they add up to target.
@@ -26,7 +29,7 @@ public class TwoSum {
     public static int[] TwoSum(int[] numbers, int target){
         int[] list = new int[2];
         
-        for (int i=0; i < numbers.length -1; i++) {
+        for (int i=0; i < numbers.length; i++) {
             for (int j=i + 1; j < numbers.length; j++) {
                 if (numbers[i] + numbers[j] == target) {
                     list[0] = i;
@@ -37,9 +40,24 @@ public class TwoSum {
         }
         return list;
     }
-    // Due to these are nested loops, the time complexity is O(n x n) = O(n^2)
+    // Due to these are nested loops, the time complexity is O(n x n) = O(n^2). Runtime = 44ms.
     // Solution, use HashMap.
 
+    public static int[] TwoSumHashMap(int[] numbers, int target){
+        Map<Integer, Integer> hashM = new HashMap<>();
 
+        for (int i= 0; i < numbers.length; i++) { 
+            int complement = target - numbers[i];
+
+            if (hashM.containsKey(complement)) {
+                return new int[]{i, hashM.get(complement)};
+            } 
+            else {
+                hashM.put(numbers[i], i);
+            }
+        }
+            return null;
+    }
+    // Time complexity of O(n). Runtime = 2ms.
 
 }
