6b3a55ecdaa49e520c424daed92213ff6799d3d6
LovisottoSantiago <santiagolovisotto@gmail.com>
2024-09-25 12:34:01 -0300

    Palindrome Number

--- Easy/PalindromeNumber.java
+++ Easy/PalindromeNumber.java
@@ -1,28 +1,40 @@
 
+import java.util.ArrayList;
+
+
 
 public class PalindromeNumber{
 
     public static void main(String[] args) {
-        int number = 121;
+        int number = 1250521;
 
         String output;
-        if(isPalindrome){
-            output = number + "is palindrome.";
+        if(isPalindrome(number)){
+            output = number + " is palindrome.";
         }
         else{
-            output = number + "is not palindrome.";
+            output = number + " is not palindrome.";
         }
-        System.out.println("This is the output: " + output);
+        System.out.println("Output: " + output);
     }
     
     public static boolean isPalindrome(int number){
+        ArrayList<Integer> list = new ArrayList<>();
+        int originalNum = number; // save the original num
+
         while (number > 0){
-            int last = number % 10; // get the last digit          
+            int last = number % 10; // get the last digit       
+            list.add(last);
             number /= 10; // remove the last digit
         }
 
+        int result = 0;
+        for (int x : list) {
+            result = result * 10 + x;
+        }
 
-        return true;
+        return result == originalNum;
     } 
-
+    // Time complexity = O(log₁₀(n)).
+    // Runtime: 6ms.
 }
